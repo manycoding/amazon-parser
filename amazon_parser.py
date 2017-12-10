1004c888588a65f88f528202598e341b4b21c9e6
manycoding <vamukhs@gmail.com>
2017-12-10 18:59:32 +0300

    Add 3d graph

--- amazon_parser.py
+++ amazon_parser.py
@@ -1,25 +1,54 @@
+import argparse
 import matplotlib.pyplot as plt
 from parser import Parser
+from mpl_toolkits.mplot3d import Axes3D
 
 
-def main():
+def draw_2d(x_values, y_values):
+    plt.figure(dpi=128, figsize=(10, 6))
+    plt.set_xlabel('$')
+    plt.set_ylabel('Reviews')
+
+    plt.scatter(x_values, y_values, edgecolor='none', s=3)
+    plt.show()
+
+
+def draw_3d(x_values, y_values, z_values):
+    plt.figure(dpi=128, figsize=(10, 6))
+    ax = plt.gca(projection='3d')
+
+    ax.set_title('3D Scatter Plot')
+    ax.set_xlabel('$')
+    ax.set_ylabel('Reviews')
+    ax.set_zlabel('Rating')
+
+    ax.scatter(x_values, y_values, z_values, c='r', marker='o')
+
+    plt.show()
+
+
+def main(type):
     p = Parser()
     b_data = p.get_data()
 
     x_values = [b['price'] for b in b_data]
     y_values = [b['reviews'] for b in b_data]
+    z_values = [b['rating'] for b in b_data]
 
     # Print the name of top item
     print(
         "The most reviewed is\n {name} {reviews} {rating} {price}".
         format(**p.top))
 
-    # Set the size of the plotting window.
-    plt.figure(dpi=128, figsize=(10, 6))
-
-    plt.scatter(x_values, y_values, edgecolor='none', s=3)
-    plt.show()
+    if type == '2d':
+        draw_2d(x_values, y_values)
+    else:
+        draw_3d(x_values, y_values, z_values)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "graph", choices=["2d", "3d"], default="2d", help="Graph type")
+    args = parser.parse_args()
+    main(args.graph)
